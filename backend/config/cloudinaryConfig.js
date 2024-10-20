da930dd81b721356163785244b4aa44a5ed1d415
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-20 13:33:24 +0300

    Add cloudinaryConfig for cloudinary API configuration

---
+++ backend/config/cloudinaryConfig.js
@@ -0,0 +1,9 @@
+const cloudinary = require('cloudinary').v2;
+
+cloudinary.config({
+    cloud_name: dcrubaesi,
+    api_key: 485422958792172,
+    api_secret: jdk0DWQzQaP_xhKIAYDoE8NG4Ic
+});
+
+module.exports = cloudinary;
