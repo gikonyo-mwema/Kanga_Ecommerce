f1593a41f1deb40972df9916fdc17717973378ef
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-20 13:37:33 +0300

    Refactor main.jsx to include Redux store and Provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux'; // Import Provider from react-redux
+import App from './App.jsx';
+import './index.css';
+import store from './redux/store'; // Import your Redux store
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}> {/* Wrap App with Provider */}
+      <App />
+    </Provider>
   </StrictMode>,
-)
+);
