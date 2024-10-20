ec3ea1ecc3c5aaffb684a9cfd403e4ce5cc6cfea
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-20 13:35:12 +0300

    Add Products page component for displaying the list of products

---
+++ frontend/src/pages/Products.jsx
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchProducts } from '../redux/slices/productSlice';
+import ProductCard from '../components/ProductCard';
+
+const Products = () => {
+  const dispatch = useDispatch();
+  
+  // Accessing products and loading/error states from Redux store
+  const products = useSelector(state => state.products.products);
+  const loading = useSelector(state => state.products.loading);
+  const error = useSelector(state => state.products.error);
+
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error}</p>;
+
+  return (
+    <div className="products-page">
+      <h1 className="text-2xl font-bold text-center mt-6">Our Products</h1>
+
+      <div className="grid grid-cols-4 gap-6 mt-8">
+        {products.length > 0 ? (
+          products.map(product => (
+            <ProductCard key={product._id} product={product} />
+          ))
+        ) : (
+          <p className="text-center">No products available.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Products;
