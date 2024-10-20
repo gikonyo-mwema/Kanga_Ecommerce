7668462205a9f1ddcaa77f42dcc1380f2545dbab
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-20 13:33:59 +0300

    Add Footer component for displaying the website footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 p-4 text-white mt-10">
+      <div className="container mx-auto text-center">
+        <p>&copy; 2024 Kanga Store. All Rights Reserved.</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
+
