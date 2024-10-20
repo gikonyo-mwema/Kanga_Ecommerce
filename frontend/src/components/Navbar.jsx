f0d1353416591ead94b7e213e81af42dc70f3bc1
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-20 13:34:36 +0300

    Add Navbar component for website navigation

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-white text-lg font-bold">
+          Kanga Store
+        </Link>
+        <ul className="flex space-x-4 text-white">
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/products">Products</Link></li>
+          <li><Link to="/cart">Cart</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
+
