54424cadbcfcaa15b96a6f703c187773a0539ecc
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-10-22 20:17:49 +0300

    Refactor productRoutes.js to add CRUD functionality and protect routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,7 +1,21 @@
 import express from 'express';
-import { getProducts } from '../controllers/productController.js';
+import {
+  getProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct
+} from '../controllers/productController.js';
+import { protect, admin } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
-router.get('/', getProducts);
+
+router.route('/')
+  .get(getProducts)
+  .post(protect, admin, createProduct);
+
+router.route('/:id')
+  .put(protect, admin, updateProduct)
+  .delete(protect, admin, deleteProduct);
+
 export default router;
 
